eea0f95ce27358e0b9a781d9d4ae1a62c77a28d0
youtl <youtl@163.com>
2019-11-04 16:30:52 +0800

    2.6.0

--- src/main/java/org/yx/db/dao/AbstractCachable.java
+++ src/main/java/org/yx/db/dao/AbstractCachable.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.yx.db;
+package org.yx.db.dao;
 
 public class AbstractCachable implements Cachable {
 
